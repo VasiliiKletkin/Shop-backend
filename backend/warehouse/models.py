3d3b4d4253497ece24c1f09c6495907b56b75671
Iulia Zagornova <zagornowwa.julia@gmail.com>
2023-10-01 14:56:11 +0400

    add warehuse and warehouse admin

---
+++ backend/warehouse/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Склад'
+        verbose_name_plural = 'Склады'
+        
+    def __str__(self):
+            return f'warehouse (self.name)'
