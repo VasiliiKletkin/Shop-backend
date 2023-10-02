6a3343f4bcf17ef728dd96207e3ad33a2ade39bc
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-10-02 13:17:08 +0400

    fix tests

--- backend/internal_api/urls.py
+++ backend/internal_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 app_name = "internal_api"
 
 urlpatterns = [
-    path('carts/', include('internal_api.carts_api.urls', namespace="carts")),
+    path('carts/', include('internal_api.carts_api.urls', namespace="carts_api")),
     path('products/', include('internal_api.products_api.urls', namespace='products')),
 ]
