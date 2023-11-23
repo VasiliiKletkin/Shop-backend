f4faf723104d4c11dccb59c3e25c46e9b821091a
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-11-23 18:49:23 +0400

    add fix

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from orders.models import Order
+from products.models import ProductItem
+from django.contrib.contenttypes.admin import GenericTabularInline
+
+
+class ProductItemInline(GenericTabularInline):
+    model = ProductItem
 
 
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ProductItemInline]
+
 
 admin.site.register(Order, OrderAdmin)
--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, ProductItem
+
 
 class ProductAdmin(admin.ModelAdmin):
     pass
 
+
+class ProductItemAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductItem, ProductAdmin)
