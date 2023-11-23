f73d2a413a626663ef8243cb9886767782bc56d7
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-11-23 18:39:14 +0400

    add fix

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
+from products.serializers import ProductItemSerializer
 
 from .models import Order
 
 
 class OrderSerializer(serializers.ModelSerializer):
     total_price = serializers.ReadOnlyField()
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    total_price = serializers.ReadOnlyField()
+    product_items = ProductItemSerializer(many=True)
     class Meta:
         model = Order
         fields = '__all__'
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -2,13 +2,19 @@ from orders.models import Order
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
-from .serializers import OrderSerializer
+from .serializers import OrderSerializer, OrderDetailSerializer
 
 
 class OrderViewSet(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    serializer_detail_class = OrderDetailSerializer
     permission_classes = [IsAuthenticated]
+    
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return self.serializer_detail_class
+        return super().get_serializer_class()
 
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
